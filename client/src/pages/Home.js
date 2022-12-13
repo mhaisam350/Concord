b93685a385c244cd7ed071f61f22c9c675df629d
mhaisam350 <mhaisam350@gmail.com>
2022-12-13 18:48:59 +0500

    Styled Home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,37 @@
-
+import styles from '../styles/Home.module.scss';
 
 export const Home = () => {
 
   return (
 
-    <div>Home
-    </div>
+    <>
+
+        <main className={styles.main}>
+            
+            <section className={styles.section}>
+
+                <h1 className={styles.logo}>Discourse</h1>
+
+                <form className={styles['homepage-form']}>
+
+                    <label for='username'>Username</label>
+                    <input type='text' name='username' required />
+
+                    <label for='rooms'>Room</label>
+                    <select name='rooms'>
+                        <option value='room1'>Room 1</option>
+                        <option value='room2'>Room 2</option>
+                    </select>
+
+                    <button type='submit'>Join Chat</button>
+
+                </form>
+
+            </section>
+
+        </main>
+
+    </>
 
   )
 
