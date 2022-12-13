a8ea8bf4d3d9a63e7d18aa10bbfa8b5995c10e12
mhaisam350 <mhaisam350@gmail.com>
2022-12-13 16:05:27 +0500

    Set up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
+
   return (
-    <div className="App">
-      Concord
-    </div>
+
+      <Routes>
+
+        <Route
+          path='/'
+          element={<Home />}
+        />
+
+      </Routes>
+
   );
+
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+
+
+export const Home = () => {
+
+  return (
+
+    <div>Home
+    </div>
+
+  )
+
+}
