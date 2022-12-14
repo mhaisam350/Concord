812f3c3b6dcea8dbe63654cbeca8ca58b892ba9e
mhaisam350 <mhaisam350@gmail.com>
2022-12-14 15:33:36 +0500

    Styled Home component

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -6,32 +6,32 @@ export const Room = () => {
 
   return (
 
-    <main>
+    <main className={styles.main}>
 
-        <section>
+        <section className={styles.section}>
 
-            <section>
+            <section className={styles['chat-header']}>
 
-                <h1>Discourse</h1>
-                <button>Leave Room</button>
+                <h1 className={styles.logo}>Discourse</h1>
+                <button className={styles['leave-btn']}>Leave Room</button>
 
             </section>
 
-            <section>
+            <section className={styles['chat-dashboard']}>
 
                 <section className={styles['chat-details']}>
                     <ChatDetails />
                 </section>
 
                 <section className={styles['chat-window']}>
-
+                    Chat window
                 </section>
                 
             </section>
 
             <section className={styles['chat-input']}>
 
-
+                Chat input
 
             </section>
 
